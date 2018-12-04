a4586ef2fd27e412c269cc7a986edc04c4f3b295
linusyoung <linusyoungrice@gmail.com>
2018-12-04 21:40:32 +1000

    add 2 moving average

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 # %%
-from bokeh.io import output_file, show
+from bokeh.io import output_file, show, save
+import bokeh
 from bokeh.plotting import figure
 from bokeh.layouts import column
 from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
 import numpy as np
 import pandas as pd
 import pandas_datareader.data as web
+
+# %%
 
 
 def get_stock(stock_code, start=None, end=None):
@@ -19,6 +22,8 @@ def get_stock(stock_code, start=None, end=None):
     """
 
     stock = web.DataReader(stock_code, 'yahoo', start=start, end=end)
+    stock['MA50'] = stock['Adj Close'].rolling(50).mean()
+    stock['MA200'] = stock['Adj Close'].rolling(200).mean()
 
     return stock
 
@@ -33,6 +38,9 @@ def plot_stock(df, stock_code, value_type='Adj Close'):
         df (DataFrame): historical stock data
         value_type (String): specify which value is used to plot
     """
+
+    output_file('../output/test.html')
+
     source = ColumnDataSource(df)
     tooltips = [
         ('Date', '@Date{%F}'),
@@ -48,10 +56,19 @@ def plot_stock(df, stock_code, value_type='Adj Close'):
 
     price = figure(y_axis_label='Stock value',
                    plot_height=500, title=stock_code)
-    price.line(x='Date', y=value_type, source=source)
+
+    adj_close = price.line(x='Date', y=value_type,
+                           source=source, legend='adj_close')
+    price.line(x='Date', y='MA50', source=source,
+               line_color='black', legend='ma50')
+    price.line(x='Date', y='MA200', source=source,
+               line_color='red', legend='ma200')
     price.grid.grid_line_alpha = 0.3
     price.xaxis.visible = False
+    hover.renderers = [adj_close]
     price.add_tools(hover)
+
+    price.legend.location = "top_left"
 
     tooltips = [
         ('Date', '@Date{%F}'),
@@ -67,12 +84,14 @@ def plot_stock(df, stock_code, value_type='Adj Close'):
     volume.line(x='Date', y='Volume', source=source)
     volume.yaxis[0].formatter = NumeralTickFormatter(format="0,0 a")
     volume.add_tools(hover)
+    volume.grid.grid_line_alpha = 0.3
+
     layout = column(children=[price, volume])
-    show(layout)
+    save(layout)
 
 
 # %%
-stock_code = 'IFN.AX'
-ifn = get_stock(stock_code)
-# print(ifn.info())
-plot_stock(ifn, stock_code)
+stock_code = 'AGL.AX'
+stock = get_stock(stock_code)
+stock.reset_index(inplace=True)
+plot_stock(stock, stock_code)
