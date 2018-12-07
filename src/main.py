24eb7521d29adb8095a431fb671f2610d5157475
linusyoung <linusyoungrice@gmail.com>
2018-12-07 19:17:35 +1000

    add bollinger band

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 from bokeh.io import output_file, show, save
 import bokeh
 from bokeh.plotting import figure
-from bokeh.layouts import column
+from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
 import numpy as np
 import pandas as pd
@@ -22,8 +22,14 @@ def get_stock(stock_code, start=None, end=None):
     """
 
     stock = web.DataReader(stock_code, 'yahoo', start=start, end=end)
-    stock['MA50'] = stock['Adj Close'].rolling(50).mean()
-    stock['MA200'] = stock['Adj Close'].rolling(200).mean()
+    # BOLLINGER BAND
+    stock['MID_BAND'] = stock['Adj Close'].rolling(20).mean()
+    stock['UPPER_BAND'] = stock['MID_BAND'] + \
+        stock['Adj Close'].rolling(20).std().mul(2)
+    stock['LOWER_BAND'] = stock['MID_BAND'] - \
+        stock['Adj Close'].rolling(20).std().mul(2)
+    stock['SMA50'] = stock['Adj Close'].rolling(50).mean()
+    stock['SMA200'] = stock['Adj Close'].rolling(200).mean()
     stock['H14'] = stock['High'].rolling(14).max()
     stock['L14'] = stock['Low'].rolling(14).min()
     stock['%K'] = 100 * (stock['Close']-stock['L14']) / \
@@ -50,7 +56,9 @@ def plot_stock(df, stock_code, value_type='Adj Close'):
     tooltips = [
         ('Date', '@Date{%F}'),
         ('High', '@High'),
-        ('Low', '@Low')
+        ('Low', '@Low'),
+        ('Upper', '@UPPER_BAND'),
+        ('Lower', '@LOWER_BAND')
     ]
     formatters = {
         'Date': 'datetime'
@@ -64,10 +72,17 @@ def plot_stock(df, stock_code, value_type='Adj Close'):
 
     adj_close = price.line(x='Date', y=value_type,
                            source=source, legend='adj_close')
-    price.line(x='Date', y='MA50', source=source,
-               line_color='black', legend='ma50')
-    price.line(x='Date', y='MA200', source=source,
-               line_color='red', legend='ma200')
+    price.line(x='Date', y='SMA50', source=source,
+               line_color='black', legend='sma50')
+    price.line(x='Date', y='SMA200', source=source,
+               line_color='red', legend='sma200')
+    price.line(x='Date', y='MID_BAND', source=source,
+               line_color='pink', legend='bb(20,2)', line_dash='dashed')
+    price.line(x='Date', y='UPPER_BAND', source=source,
+               line_color='pink')
+    price.line(x='Date', y='LOWER_BAND', source=source,
+               line_color='pink')
+
     price.grid.grid_line_alpha = 0.3
     price.xaxis.visible = False
     hover.renderers = [adj_close]
@@ -98,18 +113,20 @@ def plot_stock(df, stock_code, value_type='Adj Close'):
     so.line(x='Date', y=80, source=source, line_color='black')
     so.line(x='Date', y=20, source=source, line_color='black')
     so.legend.location = 'top_left'
-    layout = column(children=[price, volume, so])
+    layout = column(price, volume, so)
     save(layout)
 
 
 # %%
 stock_code = 'AGL.AX'
 stock = get_stock(stock_code)
+# trunc data to 1 year
+stock = stock['2018']
 stock.reset_index(inplace=True)
 
 # %%
-flter = ['High', 'Low', 'Adj Close', 'H14', 'L14', '%K', '%D']
-print(stock[flter].head(20))
+# flter = ['High', 'Low', 'Adj Close', 'H14', 'L14', '%K', '%D']
+# print(stock[flter].head(20))
 
 # %%
 plot_stock(stock, stock_code)
